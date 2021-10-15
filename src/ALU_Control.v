d5ed30c50cda2b4a5bcdcc16338d850c68825ff7
mauper20 <Mauper_osorio@hotmail.com>
2021-10-14 22:49:42 -0500

    Update ALU_Control.v

--- src/ALU_Control.v
+++ src/ALU_Control.v
@@ -31,16 +31,13 @@ localparam R_Type_OR       =7'b0_000_110;
 localparam R_Type_AND      =7'b0_000_111;
 localparam R_Type_SLL      =7'b0_000_001;
 localparam R_Type_SRL      =7'b0_000_101;
-localparam R_Type_SRA      =7'b1_000_101;
 localparam I_Type_ADDI     =7'bx_001_000;
 localparam I_Type_XORI     =7'bx_001_100;
 localparam I_Type_ORI      =7'bx_001_110;
 localparam I_Type_ANDI     =7'bx_001_111;
 localparam I_Type_SLLI     =7'bx_001_001;
 localparam I_Type_SRLI     =7'bx_001_101;
-localparam I_Type_SRAI     =7'b1_001_101;
-localparam I_Type_SLTI     =7'bx_001_010;
-localparam I_Type_ORI      =7'bx_010_XXX;
+localparam U_Type_LUI      =7'bx_010_XXX;
 
 reg [3:0] alu_control_values;
 wire [6:0] selector;
@@ -50,14 +47,19 @@ assign selector = {funct7_i, ALU_Op_i, funct3_i};
 always@(selector)begin
 	casex(selector)
 		R_Type_ADD:    alu_control_values= 4'b00_00;
-	   R_Type_SUB:    alu_control_values= 4'b00_00;
-		R_Type_XOR:    alu_control_values= 4'b00_00;
-		R_Type_OR:     alu_control_values= 4'b00_00;
+	   R_Type_SUB:    alu_control_values= 4'b00_01;
+		R_Type_XOR:    alu_control_values= 4'b00_10;
+		R_Type_OR:     alu_control_values= 4'b00_11;
 		R_Type_AND:    alu_control_values= 4'b00_00;
 		R_Type_SLL:    alu_control_values= 4'b00_00;
 		R_Type_SRL:    alu_control_values= 4'b00_00;
-		R_Type_SRA:    alu_control_values= 4'b00_00;
 		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		I_Type_ADDI:   alu_control_values= 4'b00_00;
+		U_Type_LUI:    alu_control_values= 4'b00_00;
 
 		default: alu_control_values = 4'b00_00;
 	endcase
