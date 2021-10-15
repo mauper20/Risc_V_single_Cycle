f6938b0b53283aa08ffc91c869dae03e61d2c7bb
mauper20 <Mauper_osorio@hotmail.com>
2021-10-15 01:06:23 -0500

    Update Immediate_Unit.v

--- src/Immediate_Unit.v
+++ src/Immediate_Unit.v
@@ -25,8 +25,11 @@ always@(op_i,Instruction_bus_i) begin
 
 	if(op_i == 7'h13)
 		Immediate_o = {{20{Instruction_bus_i[31]}},Instruction_bus_i[31:20]};// I format
-	else
-		Immediate_o = 0;// U format
+		else
+			if(op_i == 7'h37)
+				Immediate_o = {{12{Instruction_bus_i[31]}},Instruction_bus_i[31:12]};// U format
+				else
+					Immediate_o = 0;// U format
 end
 
 
