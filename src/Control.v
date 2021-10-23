4563870e8b94fec49921380928d1edd0d7d8e06c
mauper20 <Mauper_osorio@hotmail.com>
2021-10-23 00:38:18 -0500

    Update Control.v

--- src/Control.v
+++ src/Control.v
@@ -28,6 +28,8 @@ module Control
 localparam R_Type           =7'h33;
 localparam I_Type_LOGIC     =7'h13;
 localparam U_Type_LUI       =7'h37;
+localparam S_Type_SW        =7'h23;
+localparam I_Mem_Type_LW    =7'h03;
 
 reg [8:0] control_values;
 
@@ -36,7 +38,9 @@ always@(OP_i) begin
 	
 		R_Type:         control_values= 9'b001_00_0_000;
 		I_Type_LOGIC:   control_values= 9'b001_00_1_001;
-		U_Type_LUI:   	 control_values= 9'b001_00_1_010;
+		U_Type_LUI:   	 control_values= 9'b001_00_1_010;	
+		S_Type_SW:   	 control_values= 9'b001_01_1_011;
+	   I_Mem_Type_LW:  control_values= 9'b011_10_1_100;
 		
 		
 
@@ -45,19 +49,19 @@ always@(OP_i) begin
 		endcase
 end	
 
-assign Branch_o = control_values[8];
+assign Branch_o     = control_values[8];
 
 assign Mem_to_Reg_o = control_values[7];
 
-assign Reg_Write_o = control_values[6];
+assign Reg_Write_o  = control_values[6];
 
-assign Mem_Read_o = control_values[5];
+assign Mem_Read_o   = control_values[5];
+ 
+assign Mem_Write_o  = control_values[4];
 
-assign Mem_Write_o = control_values[4];
+assign ALU_Src_o    = control_values[3];
 
-assign ALU_Src_o = control_values[3];
-
-assign ALU_Op_o = control_values[2:0];	
+assign ALU_Op_o     = control_values[2:0];	
 
 endmodule
 
