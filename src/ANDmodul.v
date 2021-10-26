d90fa8c2df2e7617fc50d04bb8a56f8d9a04b73f
mauper20 <Mauper_osorio@hotmail.com>
2021-10-25 20:17:19 -0500

    Create ANDmodul.v

---
+++ src/ANDmodul.v
@@ -0,0 +1,24 @@
+
+/******************************************************************
+* Description
+*	and logica
+*	1.0
+* Author:
+*	Adrian Guevara, Mauricio Peralta
+* email:
+*	mperalta.osorio@iteso.mx
+* Date:
+*	25/10/2021
+******************************************************************/
+
+module ANDGate
+(
+	input A,
+	input B,
+	output C
+);
+
+
+assign C = A & B;
+
+endmodule
