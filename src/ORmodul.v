9cf0c73d775a27e37b98b18fb8e0c6de9457b241
mauper20 <Mauper_osorio@hotmail.com>
2021-10-25 22:53:50 -0500

    Create ORmodul.v

---
+++ src/ORmodul.v
@@ -0,0 +1,23 @@
+
+/******************************************************************
+* Description
+*	and logica
+*	1.0
+* Author:
+*	Adrian Guevara, Mauricio Peralta
+* email:
+*	mperalta.osorio@iteso.mx
+* Date:
+*	25/10/2021
+******************************************************************/
+module ORGate
+(
+	input A,
+	input B,
+	output C
+);
+
+
+assign C = A | B;
+
+endmodule
