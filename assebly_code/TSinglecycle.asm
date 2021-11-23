77d67bb74559263ffe2b08de3035a7d66df00192
mauper20 <Mauper_osorio@hotmail.com>
2021-11-22 23:48:30 -0600

    Update TSinglecycle.asm

--- assebly_code/TSinglecycle.asm
+++ assebly_code/TSinglecycle.asm
@@ -1,19 +1,8 @@
-        add zero,zero,zero
-        add zero,zero,zero
-        add zero,zero,zero
  	lui  s0,      0x10010
- 	add zero,zero,zero
- 	add zero,zero,zero
  	ori  s1, s0,  0x24
  	addi s2, zero,1
  	addi s3, zero,32
- 	add zero,zero,zero
  	slli t0, s2,  4
  	srli t1, s3,  4
- 	add zero,zero,zero
- 	add zero,zero,zero
  	sub  t2, t0,  t1
-	add zero,zero,zero
-        add zero,zero,zero
-	add zero,zero,zero
-        add zero,zero,zero
+
