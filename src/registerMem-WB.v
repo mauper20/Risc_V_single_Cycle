1c2dae2ca1d294102b308af824734e25165647b4
mauper20 <Mauper_osorio@hotmail.com>
2021-11-29 18:56:58 -0600

    Update registerMem-WB.v

--- src/registerMem-WB.v
+++ src/registerMem-WB.v
@@ -24,14 +24,19 @@ module RegisterMEM_WB
 	input [4:0]  RD_in,
 	input [31:0] ReadData_in,
 	input [31:0] ALU_result_in,
-
-	output reg [70:0]DataOutMEM_WB
+	input Flush,
+	input jal_in,
+	
+	output reg [72:0]DataOutMEM_WB
 );
-wire [70:0] datos;
-assign datos = {RegWrite_in,MemToReg_in,RD_in,ReadData_in,ALU_result_in};
+wire [72:0] datos;
+assign datos = {jal_in,MemWrite_in,RegWrite_in,MemToReg_in,RD_in,ReadData_in,ALU_result_in};
 
 always@(negedge reset or negedge clk) 
-	begin
+	begin	
+		/*if(Flush==1)
+			DataOutMEM_WB<=initvalue;
+		else*/
 		if(reset==0)
 			DataOutMEM_WB<=initvalue;
 		
