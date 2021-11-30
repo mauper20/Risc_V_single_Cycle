1921e7454e6958369c488ceba837f4f18491b51e
mauper20 <Mauper_osorio@hotmail.com>
2021-11-29 18:57:38 -0600

    Create Multiplexer_2_to_1_RegW_Jalr_or_SW.v

---
+++ src/Multiplexer_2_to_1_RegW_Jalr_or_SW.v
@@ -0,0 +1,20 @@
+
+
+module Multiplexer_2_to_1_RegW_Jalr_or_SW
+(   
+	input Selector_i,
+	input  Mux_Data_0_i,
+	input  Mux_Data_1_i,
+	
+	output reg Mux_Output_o
+
+);
+
+	always@(Selector_i ,Mux_Data_1_i ,Mux_Data_0_i) begin
+		if(Selector_i)
+			Mux_Output_o = Mux_Data_1_i;
+		else
+			Mux_Output_o = Mux_Data_0_i;
+	end
+
+endmodule
