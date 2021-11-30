a974b152319c3e69d2be3e8f0a46e7add716019e
mauper20 <Mauper_osorio@hotmail.com>
2021-11-29 23:53:47 -0600

    Update Program_Memory.v

--- src/Program_Memory.v
+++ src/Program_Memory.v
@@ -31,7 +31,7 @@ assign real_address = {2'b0, Address_i[16:2]};
 
 	initial
 	begin
-		$readmemh("C:/Arqui/Risc_V_single_Cycle/src/HW.dat", rom);
+		$readmemh("C:/ACP_proj/Risc_V_single_Cycle/src/HTw.dat", rom);
 	end
 
 	always @ (real_address)
