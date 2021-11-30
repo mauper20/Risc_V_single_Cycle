273fcc041b30d14a409c42afc12a2ede7742ce2f
mauper20 <Mauper_osorio@hotmail.com>
2021-11-29 18:57:02 -0600

    Update registerIF-ID.v

--- src/registerIF-ID.v
+++ src/registerIF-ID.v
@@ -18,25 +18,36 @@ module RegisterIF_ID
 	input clk,
 	input reset,
 	input enable,
-	input [N-1:0]pc_in,
-	input [N-1:0] DataInput,
+	input [31:0]pc_in,
+	input [31:0] DataInput,
+	input Flush,
+	input [31:0]PC,
+	input [31:0]PCplusI,
+	input [31:0]PCplus4,
 	
-	
-	output reg [N-1:0] DataOutput,
-	output reg [N-1:0] pc_out
+	output reg [127:0] DataOutput,
+	output reg [31:0] pc_out
 );
+wire [127:0] datos;
+assign datos = {PCplus4,PC,PCplusI,DataInput};
 
 always@(negedge reset or negedge clk) 
-	begin
+	begin 
 			if(reset==0)
 				begin
 					DataOutput <= initvalue;
-					pc_out<= initvalue;
+					pc_out<= pc_in;
+				end
+			else
+			if(Flush==1)
+				begin
+					DataOutput <= 0;//initvalue;
+					pc_out<= pc_in;
 				end
 			else	
-			if(enable==1)
+			if(enable==0)//0
 				begin
-					DataOutput<=DataInput;
+					DataOutput<=datos;
 					pc_out<= pc_in;
 				end
 	end
